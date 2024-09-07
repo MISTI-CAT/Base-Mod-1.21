e467f93b92d62db6be36268edb0997c6cd3b6bbd
MISTI-CAT <yanaisegal200@gmail.com>
2024-09-07 15:12:02 +0300

    added Base for blocks

---
+++ src/main/java/net/yanay/basemod/block/ModBlocks.java
@@ -0,0 +1,27 @@
+package net.yanay.basemod.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.yanay.basemod.BaseMod;
+
+public class ModBlocks {
+
+
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, Identifier.of(BaseMod.MOD_ID, name), block);
+    }
+
+    private static void registerBlockItem(String name, Block block) {
+        Registry.register(Registries.ITEM, Identifier.of(BaseMod.MOD_ID, name),
+                new BlockItem(block, new Item.Settings()));
+    }
+
+    public static void registerModBlocks() {
+        BaseMod.LOGGER.info("Registering Mod Blocks for " + BaseMod.MOD_ID);
+    }
+}
